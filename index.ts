a9c40ee02edc439b4ef37b7da4918df7d100dc23
malt9830 <malt9830@stud.kea.dk>
2024-03-07 13:59:28 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,21 +1,23 @@
-import express from "express";
-import dbConnect from "./startup/dbConnect";
-import "dotenv/config";
-import todoRouter from "./routers/todoRouter";
-import cors from "cors";
+import express from 'express'
+import dbConnect from './startup/dbConnect'
+import 'dotenv/config'
+import cors from 'cors'
+import setupRouters from './startup/setupRouters'
 
-dbConnect();
+dbConnect()
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use("/todos", todoRouter);
+app.use(cors())
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+dbConnect()
+setupRouters(app)
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+  console.log('Server is running on port 3000')
+})
